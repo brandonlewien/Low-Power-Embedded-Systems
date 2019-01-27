c3b3464ee635331cc20b748083b13650346ff125
brandonlewien <brle6988@colorado.edu>
2019-01-26 18:43:52 -0700

    WIP Sleep, enum and stuff

--- sleep.c
+++ sleep.c
@@ -1,12 +1,25 @@
 #include "sleep.h"
 #include <em_core.h>
 
+typedef enum{
+	EnergyMode0 = 0,
+	EnergyMode1 = 1,
+	EnergyMode2 = 2,
+	EnergyMode3 = 3,
+	EnergyMode4 = 4
+} EM;
+
 void Sleep_Block_Mode(unsigned int EM){
+	//CORE_ENTER_CRITICAL();
 
 }
 void Sleep_UnBlock_Mode(unsigned int EM){
 
 }
 void Enter_Sleep(void){
+	CORE_DECLARE_IRQ_STATE;
+	//SLEEP_EnergyMode_t allowedEM;
 	CORE_ENTER_CRITICAL();
+	//Need to check if we can get to the energy mode state
+
 }
