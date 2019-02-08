cbb45caf05cb5b9bc8cbac398645504d8f3affd0
Sonya11 <sonya.schuppan@colorado.edu>
2019-02-07 17:38:12 -0700

    i2c clock

--- cmu.c
+++ cmu.c
@@ -2,14 +2,15 @@
 
 void cmu_letimer_init(void){
 
-	CMU_ClockSelectSet(cmuClock_HFPER, cmuSelect_HFXO);
-	CMU_ClockEnable(cmuClock_HFPER, true);
+	CMU_ClockSelectSet(cmuClock_HFPER, cmuSelect_HFXO);		//route HFXO to HFPERCLK(high freq peripheral clk) 	(I2C clock tree 1)
+	CMU_ClockEnable(cmuClock_HFPER, true);					//enable HFPERCLK									(I2C clock tree 2)
+
 															// By default, LFRCO is enabled
 	CMU_OscillatorEnable(cmuOsc_LFRCO, false, false);		// using LFXO or ULFRCO
 															// Route LF clock to the LF clock tree
 	CMU_OscillatorEnable(cmuOsc_ULFRCO, false, false);
 	CMU_OscillatorEnable(cmuOsc_LFXO, true, true);
-	CMU_ClockSelectSet(cmuClock_LFA, cmuSelect_LFXO);		// routing clock to LFA
+	CMU_ClockSelectSet(cmuClock_LFA, cmuSelect_LFXO);		// routing LFXO to LFA								(LETIMER clock tree 1)
 
 	CMU_ClockEnable(cmuClock_LFA, true);
 	CMU_ClockEnable(cmuClock_CORELE, true);
@@ -20,6 +21,8 @@ void cmu_letimer_init(void){
 
 
 void cmu_i2c_init(void){
+////
+
 	CMU_OscillatorEnable(cmuOsc_HFXO, true, true);				// enables HFXO clock
 	CMU_ClockSelectSet(cmuClock_I2C0, cmuSelect_HFXO);      // routes I2C clock with HFXO
 	CMU_ClockEnable(cmuClock_I2C0, true); 					// enable I2C clock
