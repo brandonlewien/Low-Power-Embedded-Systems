cc6ada60f2b444fe782466d56429472d982caef0
brandonlewien <brle6988@colorado.edu>
2019-02-26 14:54:50 -0700

    uart implementation

--- uart.c
+++ uart.c
@@ -1,9 +1,22 @@
-/*
- * uart.c
- *
- *  Created on: Feb 26, 2019
- *      Author: Brandon
- */
-
 #include "uart.h"
 
+void uart_init(void) {
+	LEUART_Init_TypeDef UART_Init_Struct;
+	UART_Init_Struct.enable   = UART_DISABLE;
+	UART_Init_Struct.refFreq  = UART_REF_FREQ;
+	UART_Init_Struct.baudrate = UART_BAUD_RATE;
+	UART_Init_Struct.databits = UART_DATA_BITS;
+	UART_Init_Struct.parity   = UART_PARITY;
+	UART_Init_Struct.stopbits = UART_STOP_BITS;
+	LEUART_Init(LEUART0, &UART_Init_Struct);
+
+	LEUART0->ROUTELOC0 = LEUART_ROUTELOC0_RXLOC_LOC8
+			           | LEUART_ROUTELOC0_TXLOC_LOC8;
+	LEUART0->ROUTEPEN  = LEUART_ROUTEPEN_RXPEN
+					   | LEUART_ROUTEPEN_TXPEN;
+
+	GPIO_PinModeSet(TX_PORT, TX_PIN, gpioModeDisabled, UART_OFF);
+	GPIO_PinModeSet(RX_PORT, RX_PIN, gpioModeDisabled, UART_OFF);
+
+	LEUART_Enable(LEUART0, true);
+}
--- uart.h
+++ uart.h
@@ -1,14 +1,56 @@
-/*
- * uart.h
+/**************************************************************************//**
+ * @file cmu.h
+ * @brief Clock management unit header
+ * @author Silicon Labs
+ * @version 1.00
+ ******************************************************************************
+ * @section License
+ * <b>(C) Copyright 2014 Silicon Labs, http://www.silabs.com</b>
+ *******************************************************************************
  *
- *  Created on: Feb 26, 2019
- *      Author: Brandon
- */
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ * DISCLAIMER OF WARRANTY/LIMITATION OF REMEDIES: Silicon Labs has no
+ * obligation to support this Software. Silicon Labs is providing the
+ * Software "AS IS", with no express or implied warranties of any kind,
+ * including, but not limited to, any implied warranties of merchantability
+ * or fitness for any particular purpose or warranties against infringement
+ * of any proprietary rights of a third party.
+ *
+ * Silicon Labs will not be liable for any consequential, incidental, or
+ * special damages, or any other relief, or for any claim by any third party,
+ * arising from your use of this Software.
+ *
+ ******************************************************************************/
 
 #ifndef SRC_UART_H_
 #define SRC_UART_H_
 
-class uart {
-};
+#include <em_leuart.h>
+#include <em_gpio.h>
+
+
+#define UART_BAUD_RATE  9600
+#define UART_DATA_BITS  leuartDatabits8
+#define UART_ENABLE     leuartEnable
+#define UART_DISABLE    leuartDisable
+#define UART_PARITY     leuartNoParity
+#define UART_REF_FREQ   0
+#define UART_STOP_BITS  leuartStopbits1
+#define UART_OFF        0
+
+#define TX_PORT         gpioPortC
+#define TX_PIN          8
+#define RX_PORT         gpioPortC
+#define RX_PIN          8
+void uart_init(void);
 
 #endif /* SRC_UART_H_ */
