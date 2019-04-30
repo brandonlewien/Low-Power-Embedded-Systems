b0af310bb1863670841aa5dd999d0b9b56d17248
Sonya11 <sonya.schuppan@colorado.edu>
2019-04-30 13:42:34 -0600

    formatting

--- cryotimer.c
+++ cryotimer.c
@@ -3,35 +3,38 @@
 
 extern uint8_t schedule_event;
 
+
+
+
 void CRYOTIMER_setup(void){
 
 	CRYOTIMER_Init_TypeDef CRYO_Init_Struct;
 
-	CRYO_Init_Struct.debugRun  = CRYO_DEBUG_DISABLE;
-	CRYO_Init_Struct.em4Wakeup = CRYO_EM4_WAKEUP;
-	CRYO_Init_Struct.enable    = CRYO_DISABLE;
-	CRYO_Init_Struct.osc       = cryotimerOscULFRCO;
-	CRYO_Init_Struct.period    = cryotimerPeriod_8;
-	CRYO_Init_Struct.presc     = cryotimerPresc_128;
+	CRYO_Init_Struct.debugRun  = CRYO_DEBUG_DISABLE;      // don't run timer during debug halt
+	CRYO_Init_Struct.em4Wakeup = CRYO_EM4_WAKEUP;         // enable wakeup from EM4
+	CRYO_Init_Struct.enable    = CRYO_DISABLE;            // don't enable timer yet
+	CRYO_Init_Struct.osc       = cryotimerOscULFRCO;      // set oscillator to ULFRCO
+	CRYO_Init_Struct.period    = cryotimerPeriod_8;       // for 1 sec period (see calculation below)
+	CRYO_Init_Struct.presc     = cryotimerPresc_128;      // for 1 sec period (see calculation below)
 	// ^Period of cryotimer wakeup events = ((2^presc)*(2^period))/cryo_freq = ((128)*(256))/32768 = 1sec :)
 
-	CRYOTIMER_Init(&CRYO_Init_Struct);
-	CRYOTIMER_Enable(CRYO_ENABLE);
+	CRYOTIMER_Init(&CRYO_Init_Struct);                    // initialize cryotimer
+	CRYOTIMER_Enable(CRYO_ENABLE);                        // enable cryotimer
 }
 
 void CRYOTIMER_Interrupt_Enable(void){
 	CRYOTIMER->IEN = 0;
-	CRYOTIMER->IEN = CRYOTIMER_IEN_PERIOD;
+	CRYOTIMER->IEN = CRYOTIMER_IEN_PERIOD;                // enable cryotimer period interrupt
 	NVIC_EnableIRQ(CRYOTIMER_IRQn);
 }
 
 
 void CRYOTIMER_IRQHandler(void) {
 	uint32_t status;
-	status = CRYOTIMER->IF & CRYOTIMER->IEN;
-	if(status & CRYOTIMER_IF_PERIOD) {
-		schedule_event |= READ_TOUCH;
-		CRYOTIMER->IFC = CRYOTIMER_IFC_PERIOD;
+	status = CRYOTIMER->IF & CRYOTIMER->IEN;               // set status to all enabled interrupts
+	if(status & CRYOTIMER_IF_PERIOD) {                     // for every PERIOD interrupt:
+		schedule_event |= READ_TOUCH;                      // set the schedule event to read the value of the cap touch sensor
+		CRYOTIMER->IFC = CRYOTIMER_IFC_PERIOD;             // clear flag
 	}
 
 }
